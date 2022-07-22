d99b8fc2aafcfd4cd6e7955dfd6e5861eab8a292
Kevin Mooney <kmooney@gmail.com>
2022-07-22 09:06:50 -0500

    numerical derivative

---
+++ numerical.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* calculate the numerical derivative of a 
+   function that takes and returns a double 
+*/
+double deriv(double fn(double), double p) {
+  double h = 0.00001;
+  return (fn(p + h) - fn(p - h)) / (2 * h);
+}
+
+int main(size_t argc, char* argv[1]) {
+  
+  double x = deriv(sin, 1.5);
+  double y = cos(-1.5);
+  printf("%f %f", x, y);
+  return EXIT_SUCCESS;
+}
